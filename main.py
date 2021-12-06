bb05bd1b4a83b605df1e58fdcf3db3fb9bbedc4a
sokal <sokalski.jakub98@gmail.com>
2021-12-06 22:12:59 +0100

    snake game completed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,16 +11,39 @@ screen.title("Snake")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey( snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
+
+
 game_on = True
 while game_on:
     screen.update()
     time.sleep(0.1)
     snake.move_snake()
+
+    #Contact with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or \
+            snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for piece in snake.snake_body[1:]:
+        if snake.head.distance(piece) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -3,6 +3,10 @@ from turtle import Turtle
 
 STARTING_POINT = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 
 class Snake():
@@ -10,32 +14,45 @@ class Snake():
     def __init__(self):
         self.snake_body = []
         self.create_snake()
+        self.head = self.snake_body[0]
 
     def create_snake(self):
         for position in STARTING_POINT:
-            new_piece = Turtle('square')
-            new_piece.color('white')
-            new_piece.penup()
-            new_piece.goto(position)
-            self.snake_body.append(new_piece)
+            self.add_piece(position)
 
     def move_snake(self):
         for piece_num in range(len(self.snake_body) - 1, 0, -1):
             new_x = self.snake_body[piece_num - 1].xcor()
             new_y = self.snake_body[piece_num - 1].ycor()
             self.snake_body[piece_num].goto(new_x, new_y)
-        self.snake_body[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.snake_body[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.snake_body[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.snake_body[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
     def right(self):
-        self.snake_body[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def add_piece(self, position):
+        """after eating food, snake gets longer"""
+        new_piece = Turtle('square')
+        new_piece.color('white')
+        new_piece.penup()
+        new_piece.goto(position)
+        self.snake_body.append(new_piece)
+
+    def extend(self):
+        """add new piece of snake"""
+        self.add_piece(self.snake_body[-1].position())
 
 
